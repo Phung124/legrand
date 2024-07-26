1c922ff9f9f7f6298c710115089e1ea932c299ea
Phung <6051071095@st.utc2.edu.vn>
2024-07-26 16:43:16 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide1');
+    const totalSlides = slides.length;
+    const slidesPerView = 4; // Number of slides to show at once
+
+    if (index >= totalSlides / slidesPerView) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = Math.ceil(totalSlides / slidesPerView) - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const newTransform = `translateX(-${currentSlide * 100}%)`;
+    document.querySelector('.slider-container-content').style.transform = newTransform;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
+
+// back-to-top
+window.addEventListener('scroll', function() {
+    const backToTopButton = document.getElementById('back-to-top');
+    if (window.scrollY > 300) {
+        backToTopButton.classList.remove('hidden');
+        backToTopButton.classList.add('visible');
+    } else {
+        backToTopButton.classList.remove('visible');
+        backToTopButton.classList.add('hidden');
+    }
+});
+
+document.getElementById('back-to-top').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+// giới han
+$(document).ready(function() {
+    var maxRows = 3;
+    var itemsPerRow = 3;
+    var totalItemsToShow = maxRows * itemsPerRow;
+
+    $('.carousel-item').each(function() {
+        var products = $(this).find('.card');
+        products.each(function(index) {
+            if (index >= totalItemsToShow) {
+                $(this).hide();
+            }
+        });
+    });
+});
+
+
